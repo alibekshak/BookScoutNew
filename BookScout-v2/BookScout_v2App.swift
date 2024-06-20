671283fa40f6b631b5d38293af77f143b48e9904
alibekshak <alibekshakiro1@gmail.com>
2024-06-20 13:38:55 +0500

    first commit

---
+++ BookScout-v2/BookScout_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  BookScout_v2App.swift
+//  BookScout-v2
+//
+//  Created by Alibek Shakirov on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookScout_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
